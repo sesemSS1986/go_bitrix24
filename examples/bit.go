9ea75c585caaba1c7510f916d4c583a5c0f79966
sesem <sesem86@mail.ru>
2025-04-06 23:19:09 +0300

    Update new files
    Signed-off-by: sesem <sesem86@mail.ru>

--- examples/bit.go
+++ examples/bit.go
@@ -9,12 +9,13 @@ const (
 	UrlWebHookIn  = "https://b24-t5rbna.bitrix24.ru/rest/1/f3s1ah2p3r1afwli/"
 	UrlWebHookOut = "UrlWebHookOut"
 	UrlAuth       = "https://oauth.bitrix.info/rest/"
-	AuthToken     = "80a0f1670077820c0077761a00000001000007ed97fc5d34b82d09a7a12e77690b9309"
-	RefreshToken  = "701f19680077820c0077761a0000000100000729cec85eef5dd68f60393c635882e9be"
+	AuthToken     = "5ae7f2670077820c0077761a00000001000007dabf86e5adfdbfaaf44e9c6fd237d033"
+	RefreshToken  = "4a661a680077820c0077761a000000010000074529eb10132f9d5f8a6e9571d3e62688"
 )
 
 func main() {
 	FullAccesReq()
+	//WebHookReq()
 }
 
 func FullAccesReq() {
@@ -22,7 +23,7 @@ func FullAccesReq() {
 	c := client.Client{&a}
 	p := client.Parameters{}
 
-	resp, err := c.Profile(true, UrlAuth, p)
+	resp, err := c.UserAdmin(true, UrlAuth, p)
 
 	if err != nil {
 		log.Println(err)
@@ -35,7 +36,7 @@ func WebHookReq() {
 	c := client.Client{}
 	p := client.Parameters{}
 
-	resp, err := c.TaskItemList(false, UrlWebHookIn, p)
+	resp, err := c.Profile(false, UrlWebHookIn, p)
 
 	if err != nil {
 		log.Println(err)
